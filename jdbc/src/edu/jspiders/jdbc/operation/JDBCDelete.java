0bf37f631900578c16b9d94d75c668b8411f9f0a
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2025-01-07 15:26:30 +0530

    Add files via upload

---
+++ jdbc/src/edu/jspiders/jdbc/operation/JDBCDelete.java
@@ -0,0 +1,39 @@
+package edu.jspiders.jdbc.operation;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCDelete {
+
+	private static Driver driver;
+	private static Connection connection;
+	private static Statement statement;
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		openConnection();
+		statement = connection.createStatement();
+		int rows = statement.executeUpdate("DELETE FROM users WHERE id = 2");
+		System.out.println(rows + " row(s) affected");
+		closeConnection();
+	}
+
+	private static void openConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		driver = new com.mysql.cj.jdbc.Driver();
+		DriverManager.registerDriver(driver);
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?user=root&password=root");
+	}
+
+	private static void closeConnection() throws SQLException {
+		if (statement != null)
+			statement.close();
+		if (connection != null)
+			connection.close();
+		if (driver != null)
+			DriverManager.deregisterDriver(driver);
+	}
+
+}
