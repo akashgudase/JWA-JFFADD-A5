3c915fcb5a2a209b36893799cfdcb8883798e859
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2025-01-16 16:47:13 +0530

    Add files via upload

---
+++ servlets/src/main/java/edu/jspiders/servlets/MakePayment.java
@@ -0,0 +1,21 @@
+package edu.jspiders.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/payment")
+public class MakePayment extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect("https://aa.phonepe.com/client/");
+	}
+
+}
